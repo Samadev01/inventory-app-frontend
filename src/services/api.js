b97821a627fbe54ef3b656744f154671ed6d0413
Samadev01 <davidore973@gmail.com>
2025-11-27 23:41:21 -0800

    Fixx API base URL for Vercel

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const API_BASE =
-  import.meta.env.VITE_API_BASE_URL ||
-  "https://inventory-app-backend-hey0.onrender.com/api";
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api";
 
 const api = axios.create({
   baseURL: API_BASE,
