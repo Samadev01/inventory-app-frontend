b93e25a2f58b969301d6ec2b4f970e4c8e8aa3a2
Samadev01 <davidore973@gmail.com>
2025-11-27 23:52:19 -0800

    Fixx API base URL for Vercell

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_BASE =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api";
+  process.env.REACT_APP_API_BASE_URL || "http://localhost:5000/api";
 
 const api = axios.create({
   baseURL: API_BASE,
